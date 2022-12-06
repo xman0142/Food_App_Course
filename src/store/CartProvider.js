1c41b0f14e66db3785eafd9570f08b7d3d4e93b5
xman0142 <44762234+xman0142@users.noreply.github.com>
2022-12-06 16:27:28 -0600

    Updated Cart Logic with context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,44 @@
+//Where we manage cart data
+
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+  if (action.type == "ADD_CART_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  return defaultCartState;
+};
+
 import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-  const removeItemFromCartHander = (id) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_CART_ITEM", item: item });
+  };
+
+  const removeItemFromCartHander = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHander
   };
